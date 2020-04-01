68c48e4dae896cb7199e7b95875461e19e8886c9
Alexandre <contact@acanoen.fr>
2020-04-01 12:13:01 +0200

    (feat) day03: Key Code Finder

--- day03/script.js
+++ day03/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('keydown', function(event) {
-    event.preventDefault();
     var locations = ['General', 'Left', 'Right'];
     document.getElementById('keyCode').innerHTML = event.keyCode;
+    document.getElementById('keyCode').setAttribute('class', 'key-code');
     document.getElementById('code').innerHTML = event.code;
     document.getElementById('location').innerHTML = locations[event.location];
     document.getElementById('key').innerHTML = event.key;
