1404ea0dcc05ceff049ae30a170d948090939634
Alexandre <contact@acanoen.fr>
2020-04-02 11:00:22 +0200

    (feat) day04: countdown

---
+++ day04/script.js
@@ -0,0 +1,29 @@
+var el_days = document.getElementById('days')
+var el_hours = document.getElementById('hours')
+var el_minutes = document.getElementById('minutes')
+var el_seconds = document.getElementById('seconds')
+
+var futureDate = new Date(2023, 1, 6, 17, 17, 0).getTime()
+
+setInterval(function() {
+    var timeRemained = futureDate - Date.now()
+    if (timeRemained < 0) {
+        clearInterval()
+        el_days.innerHTML = pad(0)
+        el_hours.innerHTML = pad(0)
+        el_minutes.innerHTML = pad(0)
+        el_seconds.innerHTML = pad(0)
+    } else {
+        el_days.innerHTML = pad(Math.floor(timeRemained / (1000 * 60 * 60 * 24)))
+        el_hours.innerHTML = pad(Math.floor((timeRemained % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+        el_minutes.innerHTML = pad(Math.floor((timeRemained % (1000 * 60 * 60)) / (1000 * 60)))
+        el_seconds.innerHTML = pad(Math.floor((timeRemained % (1000 * 60)) / 1000))
+    }
+}, 1000)
+
+function pad(number) {
+    if (number < 10) {
+        return `0${number}`
+    }
+    return number
+}
